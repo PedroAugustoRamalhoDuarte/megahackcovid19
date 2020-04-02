320548a6ea42d7ef1eaf80ee137b263c8be70037
Igor Lima <igorlima1740@gmail.com>
2020-04-02 13:30:10 -0300

    alteracoes no APP.js e organizacao do codigo

--- FrontEnd/frontend/src/components/loginComponent/login.component.js
+++ FrontEnd/frontend/src/components/loginComponent/login.component.js
@@ -1,34 +1,85 @@
 import React, { Component } from "react";
 import './loginComponent.css'
 
-export default class Login extends Component {
+class Login extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            email: "",
+            password: "",
+            errors: {}
+        }
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(userData)
+    }
+
     render() {
+        const { errors } = this.state;
         return (
-            <form>
-                <h3>Login</h3>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <form noValidate onSubmit={this.onSubmit} >
+                        <h3>Login</h3>
 
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Ex: cliente@email.com" />
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                className="form-control"
+                                placeholder="Ex: cliente@email.com"
+                                onChange={this.onChange}
+                                value={this.state.email}
+                                error={errors.email}
+                                id="email"
+                                type="email"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Senha</label>
+                            <input
+                                className="form-control"
+                                placeholder="Ex: onb23b423"
+                                onChange={this.onChange}
+                                value={this.state.password}
+                                error={errors.password}
+                                id="password"
+                                type="password"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                            </div>
+                        </div>
+
+                        <button
+                            className="btn btn-light btn-block"
+                            type="submit"
+                        >
+                            Login
+                        </button>
+                        <p className="forgot-password text-right">
+                            Forgot <a href="http://libgen.is/">password?</a>
+                        </p>
+                    </form>
                 </div>
-
-                <div className="form-group">
-                    <label>Senha</label>
-                    <input type="password" className="form-control" placeholder="Ex: onb23b423" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-light btn-block">Login</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
+            </div>
         );
     }
 }
+
+export default Login;
---
+++ FrontEnd/frontend/src/layout/Landing.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import {Link} from "react-router-dom"
+
+class Landing extends Component{
+    render() {
+        return(
+            <h1>Landing page</h1>
+        )
+    }
+}
+
+export default Landing;
