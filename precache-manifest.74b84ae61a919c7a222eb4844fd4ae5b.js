25c81292662745ec4ab587c886b78280c74cedbf
Igor Lima <igorlima1740@gmail.com>
2020-04-07 17:03:02 -0300

    Updates

---
+++ precache-manifest.74b84ae61a919c7a222eb4844fd4ae5b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "1834985d5c4d3d322aeaec021c8ce8d4",
+    "url": "/megahackcovid19/static/media/coins.1834985d.png"
+  },
+  {
+    "revision": "01481219fb1dc5cc1cb6",
+    "url": "/megahackcovid19/static/css/main.9ddfb611.chunk.css"
+  },
+  {
+    "revision": "be2ed1511f856f57648ebb4d1c0a1b96",
+    "url": "/megahackcovid19/static/media/user-img-placeholder.be2ed151.jpeg"
+  },
+  {
+    "revision": "eb0990fcdde62377868c",
+    "url": "/megahackcovid19/static/js/1.eb0990fc.chunk.js"
+  },
+  {
+    "revision": "bcb43cc7161ba8449983",
+    "url": "/megahackcovid19/static/js/runtime~main.bcb43cc7.js"
+  },
+  {
+    "revision": "6be599be1c4fa550a289e33179d3c172",
+    "url": "/megahackcovid19/static/media/generico.6be599be.png"
+  },
+  {
+    "revision": "6e174c12453c6521efa8f35a507acb9e",
+    "url": "/megahackcovid19/static/media/dalacinC.6e174c12.png"
+  },
+  {
+    "revision": "9af87d6d812883f731b0cac654b9447d",
+    "url": "/megahackcovid19/static/media/video-call.9af87d6d.png"
+  },
+  {
+    "revision": "87e48b5579b7e0a78c70c5fe8bfc80f4",
+    "url": "/megahackcovid19/static/media/prescription.87e48b55.jpg"
+  },
+  {
+    "revision": "01481219fb1dc5cc1cb6",
+    "url": "/megahackcovid19/static/js/main.01481219.chunk.js"
+  },
+  {
+    "revision": "7b471aa8780d10cf4a7eebc5cebe694d",
+    "url": "/megahackcovid19/static/media/notification.7b471aa8.png"
+  },
+  {
+    "revision": "bafb37176b3a4ff0a9ca6f40c7fa5c2c",
+    "url": "/megahackcovid19/static/media/delivery.bafb3717.jpg"
+  },
+  {
+    "revision": "3b756edc2fe30c009ab0fb27ea863bf0",
+    "url": "/megahackcovid19/static/media/cloud.3b756edc.png"
+  },
+  {
+    "revision": "24b44e65ec8b5ea7eabb17a04fdfaebe",
+    "url": "/megahackcovid19/static/media/medicine.24b44e65.png"
+  },
+  {
+    "revision": "db52748bca8eb94ae1cdf4b0b6676f53",
+    "url": "/megahackcovid19/static/media/novalgina.db52748b.png"
+  },
+  {
+    "revision": "b5a7731f248c3ffb2b0524432e692fff",
+    "url": "/megahackcovid19/static/media/predsim.b5a7731f.jpg"
+  },
+  {
+    "revision": "98e436f1f8ee57fc15592d60090bed40",
+    "url": "/megahackcovid19/static/media/receita1.98e436f1.png"
+  },
+  {
+    "revision": "eb0990fcdde62377868c",
+    "url": "/megahackcovid19/static/css/1.60094d83.chunk.css"
+  },
+  {
+    "revision": "072ddfa29854aa7e15c2f1a1855ef0fc",
+    "url": "/megahackcovid19/index.html"
+  }
+];
